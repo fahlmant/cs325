8ee74ea8c99c26ee0e8390a6ed9edef0f0e63838
Taylor <fahlmantaylor@gmail.com>
2016-01-07 19:11:41 -0800

    Finished alg1 for assignment 1

--- assignment1/alg1.py
+++ assignment1/alg1.py
@@ -1,1 +1,15 @@
-#Some python here
+array = [31, -41, 59, 26, -53, 58, 97, -93, -23, 84]
+
+length = len(array)
+max = 0
+num = 0
+
+for j in range (0, length):
+    for i in range (0, j):
+        num = 0
+        for k in range (i, j):
+            num = num + array[k]
+            if(num > max):
+                max = num
+
+print (max)
