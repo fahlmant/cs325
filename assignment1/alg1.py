7fb6d8dea3deec62961babcfb5caa78272ecc024
armiller <anthonymiller@outlook.com>
2016-01-12 20:23:53 -0800

    Add running time

--- assignment1/alg1.py
+++ assignment1/alg1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import time
 
 tests = [
 ([22, -27, 38, -34, 49, 40, 13, -44, -13, 28, 46, 7, -26, 42, 29, 0, -6, 35, 23, -37, 10, 12, -2, 18, -12, -49, -10, 37, -5, 17, 6, -11, -22, -17, -50, -40, 44, 14, -41, 19, -15, 45, -23, 48, -1, -39, -46, 15, 3, -32, -29, -48, -19, 27, -33, -8, 11, 21, -43, 24, 5, 34, -36, -9, 16, -31, -7, -24, -47, -14, -16, -18, 39, -30, 33, -45, -38, 41, -3, 4, -25, 20, -35, 32, 26, 47, 2, -4, 8, 9, 31, -28, 36, 1, -21, 30, 43, 25, -20, -42], 239),
@@ -70,7 +71,13 @@ def run_tests(function):
         test_num = test_num + 1
 
 if __name__ == "__main__":
-    print "\nAlgorithm 1\n"
+    print "\nAlgorithm 1"
+    print "--------------"
+    start_time = time.time()
     run_tests(alg1)
-    print "\nAlgorithm 2\n"
+    print "\nRunning time: {0}".format(time.time() - start_time)
+    print "\nAlgorithm 2"
+    print "--------------"
+    start_time = time.time()
     run_tests(alg2)
+    print "\nRunning time: {0}".format(time.time() - start_time)
