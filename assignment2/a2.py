13c212a131c99ca91b0beecd6c91dad2b07d807f
armiller <anthonymiller@outlook.com>
2016-01-25 18:18:43 -0800

    Add function to parse assignment tests

--- assignment2/a2.py
+++ assignment2/a2.py
@@ -3,11 +3,10 @@
 from itertools import combinations
 from functools import reduce
 
-
-NUM_LOCKERS = 0
-NUM_KEYS = 0
-NUM_TENNIS = 0
-KEY_ARRAY = TENNIS_ARRAY = []
+# NUM_LOCKERS = 0
+# NUM_KEYS = 0
+# NUM_TENNIS = 0
+# KEY_ARRAY = TENNIS_ARRAY = []
 
 def run_tests(function):
     with open('./dp.txt', 'r') as f:
@@ -25,39 +24,46 @@ def run_tests(function):
                 print "Test {} passed".format(test_num[-1])
             f.readline()
 
-def read_input(file):
-    global NUM_LOCKERS
-    global NUM_KEYS
-    global NUM_TENNIS
-    global KEY_ARRAY
-    global TENNIS_ARRAY
-    with open(file, "r") as f:
-        nums = f.readline().split()
-        KEY_ARRAY = [int(x) for x in f.readline().split()]
-        TENNIS_ARRAY = [int(x) for x in f.readline().split()]
-        NUM_LOCKERS = int(nums[0])
-        NUM_KEYS = int(nums[1])
-        NUM_TENNIS = int(nums[2])
+def assignment_tests(test_file, function):
+    num_lockers = 0
+    num_keys = 0
+    num_tennis = 0
+    key_array = []
+    tennis_array = []
+    answers = []
+    with open(test_file, "r") as f:
+        for i in range(8):
+            f.readline()
+            nums = f.readline().split()
+            key_array = [int(x) for x in f.readline().split()]
+            tennis_array = [int(x) for x in f.readline().split()]
+            num_lockers = int(nums[0])
+            num_keys = int(nums[1])
+            num_tennis = int(nums[2])
+            answers.append(function(num_lockers, num_keys, num_tennis, key_array, tennis_array))
+            f.readline()
+
+    print "{} = {}".format(function.__name__, answers)
 
 # Algorithm 1
 def alg1(num_lockers, num_keys, num_tennis, keys, tennis):
     current_keys = []
     lengths = []
-    for i in range(1, NUM_KEYS + 1):
-        current_keys = current_keys + (list(combinations(range(0, NUM_KEYS ), i)))
+    for i in range(1, num_keys + 1):
+        current_keys = current_keys + (list(combinations(range(num_keys), i)))
 
     for comb in current_keys:
         total = 0
-        current_lockers = [0 for x in range(0,lockers)]
+        current_lockers = [0 for x in range(num_lockers)]
         for x in range(0, len(comb)):
             current_lockers[keys[comb[x]] - 1] = 1
-        for i in range(0, NUM_TENNIS):
+        for i in range(0, num_tennis):
             #For each tennis ball
             #Get the position of the tennis ball
             pos = tennis[i] - 1
             distances = []
             #For each locker
-            for j in range (0, lockers):
+            for j in range (num_lockers):
                 #If the locker is open
                 if(current_lockers[j] == 1):
                     #Find the distance between the ball and the locker
@@ -106,21 +112,6 @@ def alg2(num_lockers, num_keys, num_tennis, keys, tennis):
 
     return answer
 
-def alg_test(num_lockers, num_keys, num_tennis, keys, tennis):
-
-    left_most = min(tennis)
-    DP = []
-    total = 0
-
-    for x in range(0, num_keys):
-        DP[x] = (keys[x] - left_most) - 1
-
-
-    total = DP[-1]
-    for i in range(len(DP) - 1, 0):
-        total = total - DP[i] 
-
-    print total
-
 if __name__ == "__main__":
-    run_tests(alg_test)
+    # run_tests(alg1)
+    assignment_tests('dp_set1.txt', alg1)
