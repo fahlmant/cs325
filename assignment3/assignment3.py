3c6d05490c5a152338e3cf7b77584da5cfbecb86
armiller <anthonymiller@outlook.com>
2016-02-22 21:02:58 -0800

    Add test function and start on dc

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -1,22 +1,39 @@
 import sys
+import math
+
+def test():
+    tests = []
+    with open('test_cases_with_solutions.txt', 'r') as f:
+        for line in f:
+            tests.append(eval(line))
+
+    num = 0
+    for t in tests:
+        answer = div_and_con(t[0])
+
+        if answer[0] != t[1] or answer[1] != t[2] or answer[2] != t[3]:
+            print "test {} FAILED: Got {}, {}, {} "\
+                    "expected {}, {}, {}".format(num, answer[0], answer[1], answer[2],
+                                                 t[1], t[2], t[3])
+        else:
+            print "test {} PASSED: GOT {}, {}, {}".format(num, answer[0], answer[1], answer[2])
+
+        num = num + 1
+
 
 def summation(array, type):
-    sum = 0
+    s = 0
     sum_array = []
     if type == "prefix":
         for i in range(len(array)):
-            sum = sum + array[i]
-            sum_array.append(sum)
+            s = s + array[i]
+            sum_array.append(s)
     elif type == "suffix":
         for i in range(len(array) - 1, -1, -1):
-            sum = sum + array[i]
-            sum_array.insert(0, sum)
+            s = s + array[i]
+            sum_array.insert(0, s)
 
     return sum_array
-
-
-def div_and_con(array):
-    pass
 
 def method2(array1, array2):
     s_a1 = sorted(array1)
@@ -25,8 +42,8 @@ def method2(array1, array2):
     len1 = len(array1)
     len2 = len(array2)
 
-    print s_a1
-    print s_a2
+    # print s_a1
+    # print s_a2
 
     best = (0, 0)
     i = 0
@@ -35,19 +52,19 @@ def method2(array1, array2):
     s = sys.maxint
 
     while (i < len1 and k >= 0):
-        print "i = {}, k = {}".format(i, k)
+        # print "i = {}, k = {}".format(i, k)
         if abs(s_a1[i] + s_a2[k]) <= s:
             best = (i, k)
             s = abs(s_a1[i] + s_a2[k])
-            print "sum = {}".format(s)
+            # print "sum = {}".format(s)
 
-        print s_a1[i] + s_a2[k]
+        # print s_a1[i] + s_a2[k]
         if s_a1[i] + s_a2[k] > 0:
             k = k - 1
         else:
             i = i + 1
 
-    print s_a1[i], s_a2[k]
+    # print s_a1[i], s_a2[k]
 
     return (s, best)
 
@@ -70,13 +87,33 @@ def method3(array1, array2):
 
     return min_num
 
+def div_and_con(array):
+    if len(array) == 1:
+        return array
 
+    n = int(math.floor(len(array) / 2))
+    print n
+    l = div_and_con(array[n:])
+    r = div_and_con(array[:n])
+
+    m = method2(summation(l, 'prefix'), summation(r, 'suffix'))
+
+    # Compare left, right and suffix/prefix
+    if l[0] < r[0] and l[0] < m[0]:
+        return l
+    elif r[0] < l[0] and r < m[0]:
+        return r
+    else:
+        return m
 
 if __name__ == "__main__":
     #  array1 = [0, 5, 12, 4]
     #  array2 = [12, -2, 7, 9]
     array3 = [58, -6, 97, -93, -23]
     array4 = [31, -41, 59, 26, -53]
+
+    #  test(array3 + array4)
+    test()
 
 
     # get summations of the prefix and suffix
@@ -85,4 +122,4 @@ if __name__ == "__main__":
     #  print summation(array4, 'suffix')
 
     # print method3(summation(array1), summation(array2))
-    print method2(summation(array3, 'prefix'), summation(array4, 'suffix'))
+    #  print method2(summation(array3, 'prefix'), summation(array4, 'suffix'))
