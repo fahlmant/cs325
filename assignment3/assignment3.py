1ba28d852976c2cdf4ac70cf99248e44f934fcc6
armiller <anthonymiller@outlook.com>
2016-02-25 21:46:31 -0800

    Somewhat working. we have two tests that pass

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -1,10 +1,7 @@
 import sys
 import math
 
-ARRAY = []
-
 def test():
-    global ARRAY
     tests = []
     with open('test_cases_with_solutions.txt', 'r') as f:
         for line in f:
@@ -12,27 +9,11 @@ def test():
 
     num = 0
     for t in tests:
-        ARRAY = t[0]
-        # data = (t[0], 0, 0, 0)
-        print t[0]
-        print "\n"
-        answer = div_and_con(t[0], 0, len(t[0]))
+        question = []
+        for i in range(len(t[0])):
+            question.append((t[0][i], i))
 
-        # print t[0]
-        # print "\n"
-
-        # f_half = t[0][:(len(t[0]) / 2)]
-        # print f_half
-        # print "\n"
-        # s_half = t[0][(len(t[0]) / 2):]
-        # print s_half
-        # print "\n"
-
-        # print "method: {}, correct: {}".format(method2(f_half, s_half), (t[1], t[2], t[3]))
-        # print "method: {}, correct: {}".format(method3(f_half, s_half), (t[1], t[2], t[3]))
-        #
-        # print "-----------------------------"
-
+        answer = div_and_con(question)
 
         if answer[0] != t[1] or answer[1] != t[2] or answer[2] != t[3]:
             print "test {} FAILED: Got {}, {}, {} "\
@@ -41,13 +22,19 @@ def test():
         else:
             print "test {} PASSED: GOT {}, {}, {}".format(num, answer[0], answer[1], answer[2])
 
-
-        # if answer != t[1]:
-        #     print "test {} FAILED: Got {} expected {}".format(num, answer, t[1])
-        # else:
-            # print "test {} PASSED: GOT {}, {}, {}".format(num, answer[0], answer[1], answer[2])
-
         num = num + 1
+
+def benchmark():
+
+
+
+def solve():
+    tests = []
+    with open('test_cases_without_solutions.txt', 'r') as f:
+        for line in f:
+            test.append(eval(line))
+
+
 
 def summation(array, type):
     #  print array
@@ -55,12 +42,12 @@ def summation(array, type):
     sum_array = []
     if type == "prefix":
         for i in range(len(array)):
-            s = s + array[i]
-            sum_array.append(s)
+            s = s + array[i][0]
+            sum_array.append((s, array[i][1]))
     elif type == "suffix":
         for i in range(len(array) - 1, -1, -1):
-            s = s + array[i]
-            sum_array.insert(0, s)
+            s = s + array[i][0]
+            sum_array.insert(0, (s, array[i][1]))
 
     return sum_array
 
@@ -104,84 +91,56 @@ def method2(array1, array2):
 
 
 def method3(array1, array2):
-    print "Array 1 = {}\nArray2 = {}".format(array1, array2)
 
     array1_index = []
     array2_index = []
     # Store indexes for arrays
-    for i in range(len(array1[0])):
-        array1_index.append((array1[i], 1, i))
-    for i in range(len(array2[0])):
-        array1_index.append((array2[i], 2, i))
+    for i in range(len(array1)):
+        array1_index.append((array1[i][0], 1, array1[i][1]))
+    for i in range(len(array2)):
+        array2_index.append((-array2[i][0], 2, array2[i][1]))
 
-    # a1 = [(array1[x], 1) for x in range(len(array1_index))]
-    for x in range(len(array2_index)):
-        array2_index[x][0] = -array2_index[x][0]
-
-    # negarray = [(-x[0], x[1], x[2]) for x in array2]
-    # a2 = [(negarray[x], 2) for x in range(len(negarray))]
     biglist = array1_index + array2_index
     sorted_biglist = sorted(biglist, key=lambda x: x[0]) #by first value
-    print "biglist"
-    print sorted_biglist
-    # print "a1: {}".format(a1)
-    # print "a2: {}".format(a2)
-    # print "biglist: {}".format(biglist)
-    min_num = sys.maxint
-    if len(sorted_biglist) == 1:
-        return sorted_biglist[0]
-    smallest = (sys.maxint, 1, 1)
 
-    # (a1: 1, a2: 2)
+    best = None
 
     for x in range(len(sorted_biglist) - 1):
-        if(sorted_biglist[x][1] != sorted_biglist[x+1][1]):
-            if abs(sorted_biglist[x][0] + sorted_biglist[x+1][0]) < smallest[0]:
-                # (returns value and the two index for the closet to 0) I'm so sorry
-                smallest = (abs(sorted_biglist[x][0] + sorted_biglist[x+1][0]), sorted_biglist[x][2], (sorted_biglist[x][2] + sorted_biglist[x+1][2]))
+        if(sorted_biglist[x][1] == sorted_biglist[x+1][1]):
+            continue
 
-    return smallest
+        if best is None or abs(sorted_biglist[x][0] - sorted_biglist[x+1][0]) < abs(best[0][2] - best[1][2]):
+            # (returns value and the two index for the closet to 0)
+            best = (sorted_biglist[x], sorted_biglist[x+1], abs(sorted_biglist[x][0] - sorted_biglist[x+1][0]))
 
-def div_and_con(array, left_index, right_index):
+    if best[1][1]:
+        best = (best[1], best[0], best[2])
+
+    return (best[2], best[0][2], best[1][2])
+
+def div_and_con(array):
     """
     Basic data structure:
 
-    input: (array)
+    input: (array of tuples (value, index))
 
-    return: (sum, left index, righ index)
+    return: (sum, left_index, right_index)
+
+    Note: we are using method3
     """
-    #  print array
-    if len(array) == 1:
-        print "base case"
-        return (array[0], left_index, right_index)
+    if len(array) < 2:
+        return (abs(array[0][0]), array[0][1], array[0][1])
 
     n = int((len(array) / 2))
-    l = div_and_con(array[:n], left_index, n)
-    r = div_and_con(array[n:], n, right_index)
-    m = method2(array[l[1]:l[2]], array[r[1]:r[2]])
+    left_sum = summation(array[:n], 'prefix')
+    right_sum = summation(array[n:], 'suffix')
+    l = div_and_con(array[:n])
+    r = div_and_con(array[n:])
+    m = method3(left_sum, right_sum)
 
-    print "n = {}, l = {}, r = {}, m = {}".format(n, l, r, m)
+    best = min(l, r, m, key=lambda x: x[0])
 
-    # Compare left, right and suffix/prefix
-
-    best = min(l, r, m, key=lambda x: x[1])
-    index = (0, 0)
-
-    if l[0] == best:
-        index = l[1], l[2]
-    elif r[0] == best:
-        index = r[1], r[2]
-    else:
-        index = m[1], m[2]
-
-    # return (array[index[0]:index[1]], index[0], index[1])
     return best
-    # if l[0] < r[0] and l[0] < m[0]:
-    #     return l
-    # elif r[0] < l[0] and r < m[0]:
-    #     return r
-    # else:
-    #     return m
 
 if __name__ == "__main__":
     test()
