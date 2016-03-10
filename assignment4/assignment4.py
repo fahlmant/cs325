fa979f0b0290bf1a53a93eac3ff110b723425235
fahlmant <fahlmantaylor@gmail.com>
2016-03-10 09:17:57 -0800

    Fixed contraints and removed text line from csv

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -3,11 +3,11 @@
 from pulp import *
 import math
 
-def solve(avg, day):
-    print "avg = {}, day = {}".format(avg, day)
+def solve(f):
     prob = LpProblem("best fit curve for temps in corvallis", LpMinimize)
 
     # LP variables
+    m = LpVariable("m", 0)
     x0 = LpVariable("x0", 0)
     x1 = LpVariable("x1", 0)
     x2 = LpVariable("x2", 0)
@@ -15,29 +15,25 @@ def solve(avg, day):
     x4 = LpVariable("x4", 0)
     x5 = LpVariable("x5", 0)
 
-    # Objective
-    prob += x0 + \
-           (x1 * day) + \
-           (x2 * math.cos((2 * math.pi * day) / 365.25)) + \
-           (x3 * math.sin((2 * math.pi * day) / 365.25)) + \
-           (x4 * math.cos((2 * math.pi * day) / (365.25 * 10.7))) + \
-           (x5 * math.sin((2 * math.pi * day) / (365.25 * 10.7)))
+    prob += m
+
+    for line in f:
+        values = parse(line)
+    # Contraints
+        prob += x0 + \
+           (x1 * values[0]) + \
+           (x2 * math.cos((2 * math.pi * values[0]) / 365.25)) + \
+           (x3 * math.sin((2 * math.pi * values[0]) / 365.25)) + \
+           (x4 * math.cos((2 * math.pi * values[0]) / (365.25 * 10.7))) + \
+           (x5 * math.sin((2 * math.pi * values[0]) / (365.25 * 10.7))) - values[1] <= m
 
     # Contraints
-    prob += x0 - \
-           (x1 * day) - \
-           (x2 * math.cos((2 * math.pi * day) / 365.25)) - \
-           (x3 * math.sin((2 * math.pi * day) / 365.25)) - \
-           (x4 * math.cos((2 * math.pi * day) / (365.25 * 10.7))) - \
-           (x5 * math.sin((2 * math.pi * day) / (365.25 * 10.7))) <= avg
-
-    # Contraints
-    prob += x0 - \
-           (x1 * day) - \
-           (x2 * math.cos((2 * math.pi * day) / 365.25)) - \
-           (x3 * math.sin((2 * math.pi * day) / 365.25)) - \
-           (x4 * math.cos((2 * math.pi * day) / (365.25 * 10.7))) - \
-           (x5 * math.sin((2 * math.pi * day) / (365.25 * 10.7))) >= -avg
+        prob += -x0 - \
+           (x1 * values[0]) - \
+           (x2 * math.cos((2 * math.pi * values[0]) / 365.25)) - \
+           (x3 * math.sin((2 * math.pi * values[0]) / 365.25)) - \
+           (x4 * math.cos((2 * math.pi * values[0]) / (365.25 * 10.7))) - \
+           (x5 * math.sin((2 * math.pi * values[0]) / (365.25 * 10.7))) + values[1] <= m
 
     status = prob.solve()
     print LpStatus[status]
@@ -50,20 +46,14 @@ def solve(avg, day):
     print value(x4)
     print value(x5)
 
-
-def parse_data(file_name):
-    data = []
-    with open(file_name, 'r') as f:
-        # Remove top line
-        f.readline()
-        for line in f:
-            data.append(tuple(line.strip('\n').strip('\r').split(';')))
-
-    return data
+def parse(line):
+    new_list = line.strip("\n")
+    new_list = line.split(";")
+    new_list = new_list[7:]
+    new_list.reverse()
+    new_list = map(float, new_list)
+    return new_list
 
 if __name__ == "__main__":
-    data = parse_data('Corvallis_data.csv')
-    # print data[0][7:]
-    for i in range(10):
-        solve(float(data[i][7]), int(data[i][8]))
-    # solve(float(data[1][7]), int(data[1][8]))
+    f = open("Corvallis_data.csv")
+    solve(f)
