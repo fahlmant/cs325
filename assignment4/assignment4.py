29a69922c94e44657b17ef5b3df28e54647f8212
armiller <anthonymiller@outlook.com>
2016-03-09 16:57:12 -0800

    Work so far

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -1,32 +1,57 @@
 #!/usr/bin/env python
 
 from pulp import *
+import math
 
-def solve(x_0, x_1, x_2, x_3, x_4, x_5, day):
+def solve(avg, day):
+    print "avg = {}, day = {}".format(avg, day)
     prob = LpProblem("best fit curve for temps in corvallis", LpMinimize)
 
     # LP variables
-    x0 = LpVariable("x0", int(x_0))
-    x1 = LpVariable("x1", int(x_1))
-    x2 = LpVariable("x2", int(x_2))
-    x3 = LpVariable("x3", int(x_3))
-    x4 = LpVariable("x4", int(x_4))
-    x5 = LpVariable("x5", int(x_5))
+    x0 = LpVariable("x0", 0)
+    x1 = LpVariable("x1", 0)
+    x2 = LpVariable("x2", 0)
+    x3 = LpVariable("x3", 0)
+    x4 = LpVariable("x4", 0)
+    x5 = LpVariable("x5", 0)
 
     # Objective
     prob += x0 + \
            (x1 * day) + \
-           (x2 * math.cos((2 * math.pi * int(day)) / 365.25)) + \
-           (x3 * math.sin((2 * math.pi * int(day)) / 365.25)) + \
-           (x3 * math.sin((2 * math.pi * int(day)) / 365.25)) + \
-           (x4 * math.cos((2 * math.pi * int(day)) / (365.25 * 10.7))) + \
-           (x5 * math.sin((2 * math.pi * int(day)) / (365.25 * 10.7)))
+           (x2 * math.cos((2 * math.pi * day) / 365.25)) + \
+           (x3 * math.sin((2 * math.pi * day) / 365.25)) + \
+           (x3 * math.sin((2 * math.pi * day) / 365.25)) + \
+           (x4 * math.cos((2 * math.pi * day) / (365.25 * 10.7))) + \
+           (x5 * math.sin((2 * math.pi * day) / (365.25 * 10.7)))
 
     # Contraints
-    prob +=
+    prob += x0 - \
+           (x1 * day) - \
+           (x2 * math.cos((2 * math.pi * day) / 365.25)) - \
+           (x3 * math.sin((2 * math.pi * day) / 365.25)) - \
+           (x3 * math.sin((2 * math.pi * day) / 365.25)) - \
+           (x4 * math.cos((2 * math.pi * day) / (365.25 * 10.7))) - \
+           (x5 * math.sin((2 * math.pi * day) / (365.25 * 10.7))) <= avg
 
+    # Contraints
+    prob += x0 + \
+           (x1 * day) + \
+           (x2 * math.cos((2 * math.pi * day) / 365.25)) + \
+           (x3 * math.sin((2 * math.pi * day) / 365.25)) + \
+           (x3 * math.sin((2 * math.pi * day) / 365.25)) + \
+           (x4 * math.cos((2 * math.pi * day) / (365.25 * 10.7))) + \
+           (x5 * math.sin((2 * math.pi * day) / (365.25 * 10.7))) >= avg
 
+    status = prob.solve()
+    print LpStatus[status]
+    # print value(prob.objective)
 
+    print value(x0)
+    print value(x1)
+    print value(x2)
+    print value(x3)
+    print value(x4)
+    print value(x5)
 
 
 def parse_data(file_name):
@@ -41,4 +66,5 @@ def parse_data(file_name):
 
 if __name__ == "__main__":
     data = parse_data('Corvallis_data.csv')
-
+    # print data[0][7:]
+    solve(float(data[0][7]), int(data[0][8]))
